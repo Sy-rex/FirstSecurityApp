dfe28fa064ce99fbcacb95e25d09059cc7520373
Kirill <sobolevkirill0@gmail.com>
2025-01-10 14:00:42 +0300

    Training to using Spring Security without custom AuthProviderImpl

--- src/main/java/com/sobolev/spring/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/sobolev/spring/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,32 @@
 package com.sobolev.spring.FirstSecurityApp.config;
 
-import com.sobolev.spring.FirstSecurityApp.security.AuthProviderImpl;
+import com.sobolev.spring.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // Settings authentication
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider); // TODO
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
