1d001a19e92ce87e2ec1d43dadec216d250c0160
Kirill <sobolevkirill0@gmail.com>
2025-01-10 17:28:57 +0300

    Add registration page

--- src/main/java/com/sobolev/spring/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/sobolev/spring/FirstSecurityApp/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .requestMatchers("/auth/login", "/error", "/auth/registration").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
---
+++ src/main/java/com/sobolev/spring/FirstSecurityApp/services/RegistrationService.java
@@ -0,0 +1,23 @@
+package com.sobolev.spring.FirstSecurityApp.services;
+
+import com.sobolev.spring.FirstSecurityApp.models.Person;
+import com.sobolev.spring.FirstSecurityApp.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Transactional
+    public void register(Person person) {
+        peopleRepository.save(person);
+    }
+}
